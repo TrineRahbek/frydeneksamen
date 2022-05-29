d665ae1a6b3dcc140fce4bbb71366f5f120d241f
TrineRahbek <90824881+TrineRahbek@users.noreply.github.com>
2022-05-29 15:06:14 +0200

    Add files via upload

---
+++ eksamenfryd/js/slideshow.js
@@ -0,0 +1,24 @@
+
+
+let slideIndex = [1,1];
+/* Class the members of each slideshow group with different CSS classes */
+
+let slideId = ["mySlides1", "mySlides2", "mySlides3"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 2}
+  if (n < 2) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no]-2].style.display = "block";
+}
